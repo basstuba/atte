d18ad6f356c7a5aed31dfd05cf9199d71557abb5
nutka <tuba4080@outlook.com>
2024-03-01 18:35:58 +0100

    update

---
+++ src/app/Http/Controllers/AtteController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Time;
+use App\Models\User;
+use Carbon\Carbon;
+
+class AtteController extends Controller
+{
+    //
+}
---
+++ src/app/Models/Time.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Time extends Model
+{
+    use HasFactory;
+
+    protected $guarded=['id'];
+    protected $fillable=[
+        'user_id',
+        'date',
+        'work_start',
+        'work_end',
+        'break_time',
+    ];
+
+    public function user() {
+        return $this->belongsTo('App\models\User');
+    }
+}
